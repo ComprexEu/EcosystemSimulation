9ade584c95b392fe7ffef35f54d1d935819c3d9b
zelgith <zelgith@gmail.com>
2024-05-15 23:09:53 +0200

    Some Tweaks

--- src/main/java/com/etalonpierwotnysigmy/Main.java
+++ src/main/java/com/etalonpierwotnysigmy/Main.java
@@ -2,6 +2,6 @@ package com.etalonpierwotnysigmy;
 
 public class Main {
     public static void main(String[] args) {
-        EcosystemSimulation simulation = new EcosystemSimulation(10, 20);
+        EcosystemSimulation simulation = new EcosystemSimulation(20, 20);
     }
 }
