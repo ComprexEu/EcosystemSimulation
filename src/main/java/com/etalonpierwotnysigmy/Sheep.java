cd0cfbb645e989ff7b12b9703f6cdb8538ca5d91
Zelgith <zelgith@gmail.com>
2024-05-21 13:54:38 +0200

    changed constructor

--- src/main/java/com/etalonpierwotnysigmy/Sheep.java
+++ src/main/java/com/etalonpierwotnysigmy/Sheep.java
@@ -2,6 +2,9 @@ package com.etalonpierwotnysigmy;
 
 public class Sheep extends Herbivore{
     public Sheep(Position position) {
+        super();
+        super.setHealth(50);
+        super.setSpeed(1);
         this.position = position;
     }
 
