0c0dff211f85913ec3bc47becb073b3c569dce69
Zelgith <zelgith@gmail.com>
2024-05-31 01:47:31 +0200

    added rabies functionality

--- src/main/java/com/etalonpierwotnysigmy/entity/animal/Wolf.java
+++ src/main/java/com/etalonpierwotnysigmy/entity/animal/Wolf.java
@@ -5,7 +5,10 @@ import com.etalonpierwotnysigmy.simulation.Map;
 import com.etalonpierwotnysigmy.simulation.Position;
 import com.etalonpierwotnysigmy.simulation.Terrain;
 
+import java.util.Random;
+
 public class Wolf extends Predator {
+    private int rabies;
 
     public Wolf(Position position) {
         super();
@@ -14,10 +17,26 @@ public class Wolf extends Predator {
         maxHealth = 50;
         speed = 2;
         this.position = position;
+        rabies = 0;
     }
-
+    protected void findTargetPredator(Entity[][] entityMap, Terrain[][] terrainMap) {
+        // znalezienie celu w zależności od potrzeb
+        targetPosition = position;
+        if (metBreedingRequirements){
+            targetPosition = findLove(entityMap);
+        }
+        else if (thirst < saturation) {
+            targetPosition = findWater(terrainMap);
+        }
+        else {
+            if(rabies>=3){
+                targetPosition = findEntity(entityMap, Animal.class);
+            }
+            else targetPosition = findEntity(entityMap, Herbivore.class);
+        }
+    }
     public void findTarget(Entity[][] entityMap, Terrain[][] terrainMap) {
-        super.findTargetPredator(entityMap, terrainMap);
+        findTargetPredator(entityMap, terrainMap);
     }
 
     @Override
@@ -29,6 +48,7 @@ public class Wolf extends Predator {
     @Override
     public void updateStats(Entity[][] entityMap, Terrain[][] terrainMap) {
         super.updateStatsPredator(entityMap, terrainMap);
+        if(rabies < 3 && Math.random() < 0.1)rabies++;
     }
 
     public void breed(Entity[][] entityMap, Terrain[][] terrainMap) {
