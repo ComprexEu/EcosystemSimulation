85e535b172583635a31a1e257427ed94c16dad70
zelgith <zelgith@gmail.com>
2024-06-13 15:22:09 +0200

    cleaner code

--- src/main/java/com/etalonpierwotnysigmy/Main.java
+++ src/main/java/com/etalonpierwotnysigmy/Main.java
@@ -11,46 +11,89 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException, IOException {
-        // check if provided arguments are valid
-        int y = args.length > 0 && Integer.parseInt(args[0]) >= 3 ? Integer.parseInt(args[0]) : -1;
-        int x = args.length > 1 && Integer.parseInt(args[1]) >= 3 ? Integer.parseInt(args[1]) : -1;
-        int i = args.length > 2 && Integer.parseInt(args[2]) > 0 ? Integer.parseInt(args[2]) : -1;
-        double deerChance = args.length > 3 && Double.parseDouble(args[3]) >= 0 && Double.parseDouble(args[3]) <= 1 ? Double.parseDouble(args[3]) : -1;
-        double sheepChance = args.length > 4 && Double.parseDouble(args[4]) >= 0 && Double.parseDouble(args[4]) <= 1 ? Double.parseDouble(args[4]) : -1;
-        double lynxChance = args.length > 5 && Double.parseDouble(args[5]) >= 0 && Double.parseDouble(args[5]) <= 1 ? Double.parseDouble(args[5]) : -1;
-        double wolfChance = args.length > 6 && Double.parseDouble(args[6]) >= 0 && Double.parseDouble(args[6]) <= 1 ? Double.parseDouble(args[6]) : -1;
-        boolean print = args.length > 7 && args[7].equals("1") && x <= 70;
-        boolean save = args.length > 8 && args[8].equals("1");
-        if(args.length == 0||y == -1||x == -1||i == -1||deerChance == -1||sheepChance == -1||lynxChance == -1||wolfChance == -1){
+
+        //check if enough arguments are provided
+        if (args.length < 9) {
+            System.out.println("Niewystarczająca ilość argumentów, zapoznaj się z dokumentacją");
+            return;
+        }
+
+        // change type of argument from String to int
+        int[] intArgs = new int[5];
+        intArgs[0] = Integer.parseInt(args[0]);
+        intArgs[1] = Integer.parseInt(args[1]);
+        intArgs[2] = Integer.parseInt(args[2]);
+        intArgs[3] = Integer.parseInt(args[3]);
+        intArgs[4] = Integer.parseInt(args[4]);
+
+        // change type of argument from String to double
+        double[] doubleArgs = new double[4];
+        doubleArgs[0] = Double.parseDouble(args[5]);
+        doubleArgs[1] = Double.parseDouble(args[6]);
+        doubleArgs[2] = Double.parseDouble(args[7]);
+        doubleArgs[3] = Double.parseDouble(args[8]);
+
+        // check if provided arguments are valid and initialise variables
+        // intArgs
+        int y = intArgs[0] >= 3 ? intArgs[0] : -1;
+        int x = intArgs[1] >= 3 ? intArgs[1] : -1;
+        int i = intArgs[2] > 0 ? intArgs[2] : -1;
+        boolean print = intArgs[3] == 1 && x <= 70;
+        boolean save = intArgs[4] == 1;
+
+        // doubleArgs
+        double deerChance = doubleArgs[0] >= 0 && doubleArgs[0] <= 1 ? doubleArgs[0] : -1;
+        double sheepChance = doubleArgs[1] >= 0 && doubleArgs[1] <= 1 ? doubleArgs[1] : -1;
+        double lynxChance = doubleArgs[2] >= 0 && doubleArgs[2] <= 1 ? doubleArgs[2] : -1;
+        double wolfChance = doubleArgs[3] >= 0 && doubleArgs[3] <= 1 ? doubleArgs[3] : -1;
+
+        // end program if invalid arguments are provided
+        if(y == -1||x == -1||i == -1||deerChance == -1||sheepChance == -1||lynxChance == -1||wolfChance == -1){
             System.out.println("Wprowadzono niepoprawne dane wejściowe, zapoznaj się z dokumentacją");
             return;
         }
-        EcosystemSimulation simulation;
+
+
+        EcosystemSimulation simulation = new EcosystemSimulation(x, y, i, deerChance, sheepChance, lynxChance, wolfChance, print, save);
         List<File> listOfFiles = new ArrayList<>();
-        boolean end = false;
-        Scanner scanner = new Scanner(System.in);
-        simulation = new EcosystemSimulation(x, y, i, deerChance, sheepChance, lynxChance, wolfChance, print, save);
+
         simulation.run();
         listOfFiles.add(simulation.file);
+
+        // repeat simulation
         System.out.println("Czy chcesz powtórzyć symulację [Y/N]?");
+        boolean end = false;
+        Scanner scanner = new Scanner(System.in);
         do {
+            // check user input
+            // if 'y': repeat simulation
+            // else if 'n': end
+            // else: repeat process
+
             if (scanner.hasNextLine()) {
                 String input = scanner.nextLine();
+
                 if (input.equalsIgnoreCase("y")) {
+
                     System.out.println("Ile chcesz wykonać powtórzeń?");
-                    int intInput = scanner.nextInt();
-                    for (int j = 0; j < intInput; ++j) {
+                    int repetitions = scanner.nextInt();
+
+                    //repeats simulation given number of times
+                    for (int j = 0; j < repetitions; ++j) {
                         simulation = new EcosystemSimulation(x, y, i, deerChance, sheepChance, lynxChance, wolfChance, print, save);
                         simulation.run();
                         listOfFiles.add(simulation.file);
                     }
+
                     end = true;
-                } else if (input.equalsIgnoreCase("n")) {
+
+                } else if (input.equalsIgnoreCase("n"))
                     end = true;
-                } else System.out.println(input);
             }
         } while (!end);
         scanner.close();
+
+        // create .csv file with average results
         if(save){
             List<List<List<String>>> dataset = new ArrayList<>();
             for (File file : listOfFiles) {
@@ -60,6 +103,7 @@ public class Main {
             csvGenerator generator = new csvGenerator("wyniki_srednia");
             generator.writeData(averageData);
         }
+
         System.out.println("Zakończono symulację");
         System.exit(0);
     }
